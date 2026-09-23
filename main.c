d5f04dda1d003abcc2990977249141ec97729316
zyz4ever <2730759127@qq.com>
2026-09-23 17:01:44 +0800

    update09231701

--- main.c
+++ main.c
@@ -51,5 +51,62 @@ int main() {
     printf("long:%d\n",sizeof(long));//编译器是32位的，64位的编译器无法在32位的计算机运行
     printf("long long:%d\n",sizeof(long long));
     //字节是处理数据的基本单位
+    //整型数据的输出格式
+    printf("************************************\n");
+    short b = -123;
+    printf("short :%hd\n",b);
+    unsigned short b1 = 123;
+    printf("unsigned short:%hu\n",b1);
+
+    int c = -1234;
+    printf("int:%d\n",c);
+    unsigned int c1=1234;
+    printf("unsigned int:%u\n",c1);
+
+    long d = -12345;
+    printf("long : %ld\n",d);
+    unsigned long d1 = 12345;
+    printf("unsigned long :%lu\n",d1);
+
+    long long e = -123456;
+    printf("long long : %lld\n",e);
+    unsigned long long e1 = 123456;
+    printf("unsigned long long: %llu\n",e1);
+    /*
+     * 字符型变量用于存储一个单一字符
+     * char和unsigned char定义变量
+     * 每个字符变量都会占用1个字节(1 Byte)
+     * 用一对英文半角格式的单引号('')引用
+     * 使用%c输出
+     * */
+    printf("********************\n");
+    //字符型数据
+    char ch;
+    ch = 'A';
+    printf("char : %c\n",ch);
+    //char的本质是一个字节大小的整型
+    //字符在内存中存储时是以这个字符的ASCII编码进行存储的，ASCII码对照表如下：
+    ch = 83;
+    printf("83 : %c\n",ch);
+    printf("'S' : %d\n",ch);
+
+    /*ASCII码大致由以下两部分组成：
+     * ASCII非打印控制字符：ASCII表上数字0-31分配给了控制字符，用于控制打印机等一些外围设备。
+     * ASCII打印字符：数字32-126分配给了能在键盘上找到的字符，当查看或打印文档时就会出现。数字127代表Del命令。
+     * */
+    /*
+     * 大小写转换
+     * 大写转小写：+32/+0x20
+     * 小写转大写：-32/-0x20
+     * 整数的字符形式
+     * 整数转字符形式：+48/+'0'
+     * 字符形式转整数：-48/-'0'
+     * */
+    printf("A -> a (A+32/-0x20):%c\n",'A'+0x20);
+    printf("a -> A (a-32/-0x20):%c\n",'a'-32);
+    //整数的字符形式转整数形式
+    printf("'1'->1('1'-'0'):%d\n",'1'-'0');
+    //整数的整数形式转字符形式
+    printf("4->'4'('4'+'0'):%c\n",4+'0');
     return 0;
 }
